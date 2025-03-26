a65b6bcdb9d4efa52f8b3815c3b141d8b2c9ba37
srikanthratnala24 <ratnalasrikanth2@gmail.com>
2025-03-26 23:20:39 +0530

    roll_num added to student model

--- student/models.py
+++ student/models.py
@@ -25,6 +25,7 @@ class StudentModel(CreatedModifiedMixin):
     fee = models.DecimalField(decimal_places=2,max_digits=9,blank=True,null=True)
     discount = models.IntegerField(blank=True,null=True)
     rank = models.IntegerField(blank=True,null=True)
+    roll_num = models.IntegerField(blank=True,null=True)
 
     def __str__(self):
         return str(self.student.first_name)
