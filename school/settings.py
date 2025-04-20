d4e3251f97fec1ab1032f0c1d4f0fbccd0cc35c5
srikanthratnala24 <ratnalasrikanth2@gmail.com>
2025-04-20 22:55:02 +0530

    database added as postgres

--- school/settings.py
+++ school/settings.py
@@ -114,13 +114,23 @@ WSGI_APPLICATION = 'school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'school',
+        'USER': 'django',
+        'PASSWORD': 'mypassword',
+        'HOST': '34.228.10.130',
+        'PORT': '5432',
     }
 }
-
 AUTH_USER_MODEL = "users.User"
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
