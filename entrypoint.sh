8ffee5d7fe5ef767fd4eba2a2fcf2a1fa5f0ad06
Your Name <you@example.com>
2025-05-01 23:31:24 +0530

    fixed migrations issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,17 @@ echo "==> ✅ Entered entrypoint.sh"
 echo "==> 🔥 Deleting old migration files (excluding __init__.py)..."
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc" -delete
+echo "==> Migrations deleted..."
 
-echo "==> Making and applying migrations..."
+echo "==> 📦 Making and applying migrations..."
 python manage.py makemigrations users student teacher
-python manage.py migrate users student teacher
+python manage.py migrate users
+python manage.py migrate student
+python manage.py migrate teacher
 python manage.py makemigrations
 python manage.py migrate
 
-echo "==> Creating superuser if not exists..."
+echo "==> 👤 Creating superuser if not exists..."
 echo "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
@@ -20,8 +23,8 @@ if not User.objects.filter(email='admin@gmail.com').exists():
     User.objects.create_superuser('admin@gmail.com', '12345')
 " | python manage.py shell
 
-echo "==> Collecting static files..."
+echo "==> 🧹 Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "==> Starting server..."
+echo "==> 🚀 Starting server..."
 gunicorn school.wsgi:application --bind 0.0.0.0:8000
